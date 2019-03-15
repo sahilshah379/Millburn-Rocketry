ce22ec37b875cc87e01aa3b399b3916abbcffdb1
SahilShah518 <sahilshah379@gmail.com>
2019-03-15 00:52:48 -0400

    big oof

--- Altimeter/Altimeter.ino
+++ Altimeter/Altimeter.ino
@@ -5,22 +5,24 @@
 
 #define SDI A4
 #define SCK A5
+#define B
 #define SEALEVELPRESSURE_HPA (1019) //https://weather.us/observations/air-pressure-station.html
 
 Adafruit_BMP3XX bmp;
 
 void setup() {
-  Serial.begin(115200);
-  while (!Serial);
-  if (!bmp.begin()) {
-    while (1);
-  }
-  bmp.setTemperatureOversampling(BMP3_OVERSAMPLING_8X);
-  bmp.setPressureOversampling(BMP3_OVERSAMPLING_4X);
-  bmp.setIIRFilterCoeff(BMP3_IIR_FILTER_COEFF_3);
+    pinMode(13, OUTPUT);
+    Serial.begin(115200);
+    while (!Serial);
+    if (!bmp.begin()) {
+        Serial.println("Failed to find a BMP3 sensor");
+        while (1);
+    }
+    bmp.setTemperatureOversampling(BMP3_OVERSAMPLING_8X);
+    bmp.setPressureOversampling(BMP3_OVERSAMPLING_4X);
+    bmp.setIIRFilterCoeff(BMP3_IIR_FILTER_COEFF_3);
 }
 
 void loop() {
-  Serial.println(bmp.readAltitude(SEALEVELPRESSURE_HPA));
-  delay(2000);
+    Serial.println(bmp.readAltitude(SEALEVELPRESSURE_HPA));
 }
